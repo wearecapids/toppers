85b08643e7b6639c6de06ede86ab2ddb8e27f995
theamanshakya <theamanshakya@gmail.com>
2020-09-26 14:16:42 +0530

    Checking Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -252,9 +252,8 @@ function showModal(id) {
     var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks on the button, open the modal
-    btn.onclick = function () {
-        modal.style.display = "block";
-    }
+    modal.style.display = "block";
+
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function () {
